e83ac1a26e0438e03dc7a6739beda84cfdfbbd6c
DESKTOP-9DRHSQH\matei <matei.lazar@protonmail.com>
2021-03-21 21:38:56 +0200

    small modifications

--- app/src/main/java/m/matthew/triviaduel/ui/home/HomeFragment.kt
+++ app/src/main/java/m/matthew/triviaduel/ui/home/HomeFragment.kt
@@ -6,19 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import m.matthew.triviaduel.R
+import m.matthew.triviaduel.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
