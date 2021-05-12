4a0c97853af779b3b83ec032b21b653e7551a0df
DESKTOP-9DRHSQH\matei <matei.lazar@protonmail.com>
2021-05-12 12:50:03 +0300

    upgraded gradle

--- app/src/main/java/m/matthew/triviaduel/ui/game/GameFragment.kt
+++ app/src/main/java/m/matthew/triviaduel/ui/game/GameFragment.kt
@@ -8,18 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import m.matthew.triviaduel.R
-import m.matthew.triviaduel.databinding.GameFragmentBinding
+import m.matthew.triviaduel.databinding.FragmentGameBinding
 import m.matthew.triviaduel.ui.home.HomeViewModel
 
-class GameFragment : Fragment() {
+class GameFragment : Fragment(R.layout.fragment_game) {
 
     private val viewModel by viewModels<GameViewModel>()
-    private lateinit var binding: GameFragmentBinding
+    private lateinit var binding: FragmentGameBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        binding = GameFragmentBinding.inflate(inflater, container, false)
-
+        binding = FragmentGameBinding.inflate(inflater, container, false)
+        
 
 
         return binding.root
